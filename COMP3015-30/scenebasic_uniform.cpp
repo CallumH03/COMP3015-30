4f56ff4e3052b4b783852ffada87465d4f5751ef
CallumH03 <callum.hill-16@students.plymouth.ac.uk>
2024-03-14 20:53:33 +0000

    got blinnphong working

--- COMP3015-30/scenebasic_uniform.cpp
+++ COMP3015-30/scenebasic_uniform.cpp
@@ -6,6 +6,7 @@
 #include <string>
 using std::string;
 
+#include <sstream>
 #include <iostream>
 using std::cerr;
 using std::endl;
@@ -16,8 +17,9 @@ using glm::vec3;
 using glm::mat4;
 
 SceneBasic_Uniform::SceneBasic_Uniform() : plane(10.0f,10.0f,100,100) {
-    mesh = ObjMesh::load("../COMP3015-30/media/palmtree.obj", true);
+    mesh = ObjMesh::load("../COMP3015-30/media/palmtree.obj",true);
 }
+
 
 void SceneBasic_Uniform::initScene()
 {
@@ -28,38 +30,53 @@ void SceneBasic_Uniform::initScene()
     //model = glm::rotate(model, glm::radians(-35.0f), vec3(1.0f, 0.0f, 0.0f));
     //model = glm::rotate(model, glm::radians(15.0f), vec3(0.0f, 1.0f, 0.0f));
     projection = mat4(1.0f);
-    prog.setUniform("Light.Position", view * glm::vec4(0.0f, 0.0f, 0.0f, 0.7f));
-    prog.setUniform("Light.Ld", vec3(1.0f, 0.9f, 0.6f));
-    prog.setUniform("Light.La", vec3(0.2f, 0.2f, 0.2f));
-    prog.setUniform("Light.Ls", vec3(1.0f, 0.9f, 0.6f));
-}
 
+    float x, z;
+    for (int i = 0; i < 3; i++) {
+        std::stringstream name;
+        name << "lights[" << i << "].Position";
+        x = 2.0f * cosf((glm::two_pi<float>() / 3) * i);
+        z = 2.0f * sinf((glm::two_pi<float>() / 3) * i);
+        prog.setUniform(name.str().c_str(), view * glm::vec4(x, 1.2f, z + 1.0f, 1.0f));
+    }
+
+    prog.setUniform("lights[0].L", vec3(0.0f, 0.0f, 0.8f));
+    prog.setUniform("lights[1].L", vec3(0.0f, 0.8f, 0.0f));
+    prog.setUniform("lights[2].L", vec3(0.8f, 0.0f, 0.0f));
+
+    prog.setUniform("lights[0].La", vec3(0.0f, 0.0f, 0.2f));
+    prog.setUniform("lights[1].La", vec3(0.0f, 0.2f, 0.0f));
+    prog.setUniform("lights[2].La", vec3(0.2f, 0.0f, 0.0f));
+
+
+}
 
 void SceneBasic_Uniform::compile()
 {
-	try {
-		prog.compileShader("shader/basic_uniform.vert");
-		prog.compileShader("shader/basic_uniform.frag");
-		prog.link();
-		prog.use();
-	} catch (GLSLProgramException &e) {
-		cerr << e.what() << endl;
-		exit(EXIT_FAILURE);
-	}
+    try {
+        prog.compileShader("shader/basic_uniform.vert");
+        prog.compileShader("shader/basic_uniform.frag");
+        prog.link();
+        prog.use();
+    }
+    catch (GLSLProgramException& e) {
+        cerr << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
-void SceneBasic_Uniform::update( float t )
+void SceneBasic_Uniform::update(float t)
 {
-    
+
 }
 
 void SceneBasic_Uniform::render()
 {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    prog.setUniform("Material.Kd", vec3(1.0f, 0.5f, 0.0f));
-    prog.setUniform("Material.Ka", vec3(0.2f, 0.1f, 0.0f));
-    prog.setUniform("Material.Ks", vec3(1.0f, 0.5f, 0.0f));
+    prog.setUniform("Material.Kd", vec3(0.4f, 0.4f, 0.4f));
+    prog.setUniform("Material.Ka", vec3(0.5f, 0.5f, 0.5f));
+    prog.setUniform("Material.Ks", vec3(0.9f, 0.9f, 0.9f));
     prog.setUniform("Material.Shininess", 180.0f);
 
     model = mat4(1.0f);
@@ -76,7 +93,9 @@ void SceneBasic_Uniform::render()
     model = glm::translate(model, vec3(0.0f, -0.45f, 0.0f));
     setMatrices();
     plane.render();
-    
+
+
+
 }
 
 void SceneBasic_Uniform::resize(int w, int h)
@@ -92,5 +111,5 @@ void SceneBasic_Uniform::setMatrices() {
     mat4 mv = view * model;
     prog.setUniform("ModelViewMatrix", mv);
     prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
-    prog.setUniform("MVP", projection*mv);
+    prog.setUniform("MVP", projection * mv);
 }
